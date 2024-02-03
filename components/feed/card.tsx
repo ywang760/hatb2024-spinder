6b7e6b77890d6ee32a883f6c73a0ed3596c83169
Yutong Wang <yutong_wang5@brown.edu>
2024-02-03 16:47:45 -0500

    card ui done

--- components/feed/card.tsx
+++ components/feed/card.tsx
@@ -3,30 +3,55 @@ import { Alien } from "@/types/alien";
 import data from "../../data/alien.json";
 import Image from "next/image";
 import styles from "./Card.module.css";
+import OutboundIcon from "@mui/icons-material/Outbound";
+import ChatIcon from "@mui/icons-material/Chat";
+
 const aliens: Alien[] = data;
 
 const Card = () => {
+  // TODO: change mock data
   const alien = aliens[0];
-  console.log(alien.profilePicture);
+
+  const handleShowDetails = () => {
+    console.log("Show details");
+  };
+
+  const handleMessage = () => {
+    console.log("Message");
+  };
 
   return (
-    <div
-      className={`${styles.card} flex flex-col bg-black items-center justify-center p-2`}
-    >
-      <div className="">
-        <Image
-          src={alien.profilePicture as string} // Path to the alien's profile picture
-          alt={alien.name} // Name of the alien as alt text
-          width={500}
-          height={1200}
-          className="rounded border-radius-md" // Make the image rounded
-        />
+    <div className={`${styles.card} relative group text-zinc-300`}>
+      <Image
+        src={alien.profilePicture as string} // Path to the alien's profile picture
+        alt={alien.name + " profile picture"} // Name of the alien as alt text
+        width={500}
+        height={1200}
+        className={`${styles.card} `} // Make the image rounded
+      />
+      <div
+        className={`${styles.card} absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-60 transition duration-500`}
+      ></div>
+      <button
+        className="absolute invisible group-hover:visible inset-0 flex items-center justify-center flex-col space-y-4"
+        onClick={() => handleShowDetails()}
+      >
+        <h1 className="text-2xl">Show details</h1>
+        <OutboundIcon className="text-4xl" />
+      </button>
+      <div className="absolute invisible group-hover:visible bottom-12 left-12 p-2 space-y-2">
+        <h1 className="text-6xl font-bold pb-2">{alien.name}</h1>
+        <p className="">{alien.species}</p>
+        {/* TODO: use different fonts to differentiate */}
+        <p className="">{alien.origin}</p>
       </div>
-
-      <div className="text-center">
-        <h2 className="text-xl font-bold">{alien.name}</h2>
-        <p className="text-gray-200">{alien.species}</p>
-        <p className="text-gray-200">{alien.origin}</p>
+      <div className="absolute invisible group-hover:visible bottom-16 right-16 p-2">
+        <button
+          className="bg-red-500 rounded-full p-4"
+          onClick={() => handleMessage()}
+        >
+          <ChatIcon className="text-4xl" />
+        </button>
       </div>
     </div>
   );
