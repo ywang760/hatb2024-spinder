9ca53c5b344b75e5c58b254593ab78ac38bcfeb0
Yutong Wang <yutong_wang5@brown.edu>
2024-02-03 13:13:38 -0500

    navbar done

---
+++ components/navbar.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Link from "next/link";
+import Button from "@mui/material/Button";
+import Image from "next/image";
+import ExploreIcon from "@mui/icons-material/Explore";
+import ChatIcon from "@mui/icons-material/Chat";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+const Navbar = () => {
+  return (
+    <nav className="flex flex-col items-center w-80 h-full p-6 bg-red-500 text-white justify-around">
+      <div className="flex flex-col items-center mb-20">
+        <Image src="/logo.png" alt="Space" width={200} height={200} />
+        <span className="text-5xl mt-20">name</span>
+      </div>
+      <div className="flex flex-col gap-8 mb-8">
+        <Link
+          href="/feed"
+          className="text-white text-2xl space-x-2 flex flex-row items-center"
+        >
+          <ExploreIcon />
+          <div>Explore</div>
+        </Link>
+        <Link
+          href="/chat"
+          className="text-white text-2xl space-x-2 flex flex-row items-center"
+        >
+          <ChatIcon />
+          <div>Chat</div>
+        </Link>
+      </div>
+      <Link
+        className="mb-20 bg-red-400 hover:bg-red-600 p-6 border-2 border-white rounded-xl text-white"
+        href="/"
+        onClick={() => {
+          /* Add logout functionality here */
+        }}
+      >
+        <LogoutIcon />
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Navbar from "@/components/navbar";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <div className="flex h-screen">
+      {router.pathname !== "/" && <Navbar />}
+      <div className="flex-grow">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
