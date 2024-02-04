b17a95efdadc38f2d4c670031ca706ce24797a4f
YixiangSun <yixiang_sun@brown.edu>
2024-02-04 08:30:18 -0500

    change homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,14 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-home-bg bg-cover bg-center font-mono">
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold text-zinc-50">Welcome to our game</h1>
+        <h1 className="text-6xl font-bold text-zinc-50">You have some new matchups!</h1>
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-20 sm:w-full">
           <Link
             href="/feed"
             className="p-6 mt-6 text-center border w-72 rounded-xl hover:text-blue-600 focus:text-blue-600 transform hover:scale-110 transition-transform duration-200"
           >
             <h3 className="text-2xl font-bold text-zinc-50">
-              Start Game &rarr;
+              See profiles &rarr;
             </h3>
           </Link>
         </div>
